c465ca34cbf825217dca8b804cb69bc844b81b07
Tao li <lt1378520056@mail.ustc.edu.cn>
2020-12-30 04:43:03 +0000

    update ConstPropagation.hpp

--- include/optimization/ConstPropagation.hpp
+++ include/optimization/ConstPropagation.hpp
@@ -4,7 +4,6 @@
 #include "Constant.h"
 #include "Instruction.h"
 #include "Module.h"
-
 #include "Value.h"
 #include "IRBuilder.h"
 #include <vector>
@@ -12,9 +11,8 @@
 #include <unordered_map>
 
 // tips: 用来判断value是否为ConstantFP/ConstantInt
-ConstantFP* cast_constantfp(Value *value);
-ConstantInt* cast_constantint(Value *value);
-
+ConstantFP *cast_constantfp(Value *value);
+ConstantInt *cast_constantint(Value *value);
 
 // tips: ConstFloder类
 
@@ -26,7 +24,19 @@ public:
         Instruction::OpID op,
         ConstantInt *value1,
         ConstantInt *value2);
-    // ...
+    ConstantFP *compute(
+        Instruction::OpID op,
+        ConstantFP *value1,
+        ConstantFP *value2);
+    ConstantInt *compute(
+        FCmpInst::CmpOp op,
+        ConstantFP *value1,
+        ConstantFP *value2);
+    ConstantInt *compute(
+        CmpInst::CmpOp op,
+        ConstantInt *value1,
+        ConstantInt *value2);
+    
 private:
     Module *module_;
 };
@@ -34,8 +44,22 @@ private:
 class ConstPropagation : public Pass
 {
 public:
-    ConstPropagation(Module *m) : Pass(m) {}
+    ConstPropagation(Module *m) : Pass(m) {
+        flod = new  ConstFolder(m);               //常量折叠初始化
+        INT32_T = Type::get_int32_type(m);        //为了判断全局变量的类型
+        FLOAT_T = Type::get_float_type(m);
+    }
+    ConstantInt* setDefVal(Value *v, ConstantInt *const_val);
+    ConstantInt* getDefVal(Value *v);
     void run();
+
+private:
+    ConstFolder *flod;
+    std::map<Value *, ConstantInt *> global_def_int;
+    std::map<Value *, ConstantFP *> global_def_fp;
+    Type *INT32_T;
+    Type *FLOAT_T;
+
 };
 
 #endif
