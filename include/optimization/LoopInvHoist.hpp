39c01921391eeade2540f0f64e9db62e7e96802b
Tao li <lt1378520056@mail.ustc.edu.cn>
2021-01-03 09:06:47 +0000

    update loopinvhoist

--- include/optimization/LoopInvHoist.hpp
+++ include/optimization/LoopInvHoist.hpp
@@ -5,11 +5,18 @@
 #include "PassManager.hpp"
 #include "Module.h"
 #include "Function.h"
+#include "LoopSearch.hpp"
 #include "BasicBlock.h"
 
 class LoopInvHoist : public Pass {
+    std::set<
+        std::pair<BasicBlock *, Instruction *> >
+            invariant;
+    void findInvariants(BBset_t *loop);
+    void moveInvariantsOut(BBset_t *loop);
+    LoopSearch  *loop_searcher;
+
 public:
     LoopInvHoist(Module *m) : Pass(m) {}
-
     void run() override;
 };
