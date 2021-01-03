aa3c4b313d5b22ed8f951a98c161efb21b11af4f
Tao li <lt1378520056@mail.ustc.edu.cn>
2021-01-03 09:06:06 +0000

    update activevars

--- include/optimization/ActiveVars.hpp
+++ include/optimization/ActiveVars.hpp
@@ -14,15 +14,26 @@
 #include <queue>
 #include <fstream>
 
+#include <unordered_set>
+
 class ActiveVars : public Pass
 {
 public:
     ActiveVars(Module *m) : Pass(m) {}
+
+    void initset(Function *func);
+
     void run();
     std::string print();
+    //print use_set and def_set for cur func
+    std::string use_def_print();
 private:
     Function *func_;
-    std::map<BasicBlock *, std::set<Value *>> live_in, live_out;
+    std::map<BasicBlock *, std::unordered_set<Value *>> live_in, live_out;
+
+    std::map<BasicBlock *, std::unordered_set<Value *>> use_set, def_set, phi_op;
+    //cur BB's phi func's parameter , it is a pair for BasicBlock and var
+    std::map<BasicBlock *, std::set<std::pair<BasicBlock *, Value *>>> phi_op_pair;
 };
 
 #endif
