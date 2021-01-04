a58cdff71c78b92499bda39b866ebe58e7b4c184
Tao li <lt1378520056@mail.ustc.edu.cn>
2021-01-04 12:17:13 +0000

    update ConstPropagation.hpp

--- include/optimization/ConstPropagation.hpp
+++ include/optimization/ConstPropagation.hpp
@@ -36,7 +36,7 @@ public:
         CmpInst::CmpOp op,
         ConstantInt *value1,
         ConstantInt *value2);
-    
+
 private:
     Module *module_;
 };
@@ -44,20 +44,22 @@ private:
 class ConstPropagation : public Pass
 {
 public:
-    ConstPropagation(Module *m) : Pass(m) {
-        flod = new  ConstFolder(m);               //常量折叠初始化
-        INT32_T = Type::get_int32_type(m);        //为了判断全局变量的类型
+    ConstPropagation(Module *m) : Pass(m)
+    {
+        flod = new ConstFolder(m);         //常量折叠初始化
+        INT32_T = Type::get_int32_type(m); //为了判断全局变量的类型
         FLOAT_T = Type::get_float_type(m);
     }
     void run();
+    int global_def_func(); //全局变量在其他子函数被定值，就不能再传播
 
 private:
     ConstFolder *flod;
     std::map<Value *, ConstantInt *> global_def_int;
     std::map<Value *, ConstantFP *> global_def_fp;
+    std::map<Value *, std::pair<int, Function *>> global_def_call; //第一个int表示该全局变量在多少个函数中被定值，第二个func标志该全局变量当前在那个func中
     Type *INT32_T;
     Type *FLOAT_T;
-
 };
 
 #endif
